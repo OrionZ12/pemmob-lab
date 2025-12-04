523be857dbb4e46ffaa0f36ed07b3ca188892aea
OrionZ12 <sulungismanara@gmail.com>
2025-12-04 11:30:10 +0700

    complete harusnya

---
+++ lib/widget/gradient_background.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class GradientBackground extends StatelessWidget {
+  final Widget child;
+
+  const GradientBackground({super.key, required this.child});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            Color(0xFF10102E),
+            Color(0xFF252540),
+          ],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+        ),
+      ),
+      child: child,
+    );
+  }
+}
